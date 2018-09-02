98c30c6ed13f6a7143aba4eeae7c62c2b55f0745
dkakimoto <shinra.dai0419@gmail.com>
2018-09-02 09:55:34 +0900

    安田マージ

--- js/map.js
+++ js/map.js
@@ -3,31 +3,70 @@ var pmap;
 var smap;
 
 //ジオコーディング
-function geocording(address, pmap) {
+function geocording(index, address, map) {
   var geocoder = new Y.GeoCoder();
   var request = { query : address };
   geocoder.execute( request , function( ydf_data ) {
     if ( ydf_data.features.length > 0 ) {
       var latlng = ydf_data.features[0].latlng;
+      var name = ydf_data.features[0].name;
       var lat = latlng.lat();
       var lng = latlng.lng();
-			pin(ydf_data.features[0].name,lat,lng,pmap);
+			pin(index, name,lat,lng, map);
     }
   } );
-
 }
 
 //ピンを立てる
-function pin(name,lat,lng,pmap){
-	var pmarker = new Y.Marker(new Y.LatLng(lat, lng));
-  pmarker.bind("click", function(){
-    alert("ueeeeeeeeeeeei!!!");
+function pin(index,name,lat,lng,map){
+	var marker = new Y.Marker(new Y.LatLng(lat, lng));
+  addElement(index, name);
+  setEvent(index, marker, name);
+ 	map.addFeature(marker);
+}
+
+// 要素をHTMLに追加
+function addElement(index, name){
+  if ( name.match(/郡/) ) {
+    // nameに郡を含む場合，郡より前部分を削除
+    var cut_index = name.indexOf('郡');
+    var newname = name.slice(cut_index+1);
+  } else {
+    // 和歌山県を削除（市区町村名だけにする）
+    var newname = name.replace("和歌山県", "");
+  }
+
+  // 挿入する要素の用意
+  var insert = '<li id="'+ index +'">'+ newname +'</li>';
+
+  if ( index.match(/s/) ) {
+    // indexにsを含む場合の処理
+    $('#secret_list').append(insert);
+  } else if ( index.match(/p/) ) {
+    // indexにpを含む場合の処理
+    $('#popular_list').append(insert);
+  }
+}
+
+// イベント設定
+function setEvent(index, marker, name){
+  marker.bind("click", function(){
+    console.log(index);
+    // 色のクリア用
+    $('#secret_list').children().css({'color':'#6c7279','background-color':'#fff'});
+    $('#popular_list').children().css({'color':'#6c7279','background-color':'#fff'});
+
+    // 色を変える
+    $('#'+index).css({'color':'white','background-color':'#e84545'});
   });
-	pmap.addFeature(pmarker);
 }
 
 //マップの描画
 window.onload = function(){
+  // Listの初期化
+  $('#secret_list').html('');
+  $('#popular_list').html('');
+
   // 一個目の地図
   pmap = new Y.Map("popular_map", {
       configure : {
@@ -55,11 +94,15 @@ function set_spot(obj){
   //smap
   var secret_spot = obj.secret;
   for(var i=0;i<secret_spot.length;i++){
-    geocording(secret_spot[i], pmap);
+    // 引数sidを追加
+    var sid = 's'+ i;
+    geocording(sid, secret_spot[i], smap);
   }
   //pmap
   var popular_spot = obj.popular;
   for(var i=0;i<popular_spot.length;i++){
-    geocording(popular_spot[i], smap);
+    // 引数pidを追加
+    var pid = 'p'+ i;
+    geocording(pid, popular_spot[i], pmap);
   }
 }
