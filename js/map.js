c076f97869f22fd97319d94a7616ac6ce380997b
dkakimoto <shinra.dai0419@gmail.com>
2018-09-02 09:02:23 +0900

    diffを修正・マージ

--- js/map.js
+++ js/map.js
@@ -1,8 +1,11 @@
+//mapを定義
+var pmap;
+var smap;
+
 //ジオコーディング
 function geocording(address, pmap) {
   var geocoder = new Y.GeoCoder();
   var request = { query : address };
-
   geocoder.execute( request , function( ydf_data ) {
     if ( ydf_data.features.length > 0 ) {
       var latlng = ydf_data.features[0].latlng;
@@ -25,10 +28,8 @@ function pin(name,lat,lng,pmap){
 
 //マップの描画
 window.onload = function(){
-  //穴場と有名スポットの取得
-  var obj = getinfo();
   // 一個目の地図
-  var pmap = new Y.Map("popular_map", {
+  pmap = new Y.Map("popular_map", {
       configure : {
           scrollWheelZoom : true,
           dragging : true
@@ -37,13 +38,9 @@ window.onload = function(){
   var pcontrol = new Y.SliderZoomControlVertical();
   pmap.addControl(pcontrol);
   pmap.drawMap(new Y.LatLng(33.9292, 135.4770), 10, Y.LayerSetId.NORMAL);
-  var secret_spot = obj.secret;
-  for(var i=0;i<secret_spot.length;i++){
-    geocording(secret_spot[i], pmap);
-  }
 
   // 二個目の地図
-  var smap = new Y.Map("secret_map", {
+  smap = new Y.Map("secret_map", {
       configure : {
           scrollWheelZoom : true,
           dragging : true
@@ -52,17 +49,17 @@ window.onload = function(){
   var scontrol = new Y.SliderZoomControlVertical();
   smap.addControl(scontrol);
   smap.drawMap(new Y.LatLng(33.9292, 135.4770), 10, Y.LayerSetId.NORMAL);
+}
+
+function set_spot(obj){
+  //smap
+  var secret_spot = obj.secret;
+  for(var i=0;i<secret_spot.length;i++){
+    geocording(secret_spot[i], pmap);
+  }
+  //pmap
   var popular_spot = obj.popular;
   for(var i=0;i<popular_spot.length;i++){
     geocording(popular_spot[i], smap);
   }
-}
-
-//穴場と有名どころの取得
-function getinfo(){
-  var obj ={
-    secret: ["和歌山県白浜町","和歌山県海南市","和歌山県田辺市","和歌山県太地町","和歌山県岩出市"],
-    popular: ["和歌山県紀の川市","和歌山県和歌山市","和歌山県新宮市","和歌山県みなべ町","和歌山県串本町"]
-  };
-  return obj;
 }
---
+++ js/prefecture_diff.js
@@ -0,0 +1,98 @@
+//都道府県リストを取得
+var cities = [];
+var diff_secpop = [];
+
+//市町村コードを取得
+function citycode_get(prefcode){
+  $.ajax({
+      url:"https://opendata.resas-portal.go.jp/api/v1/cities?prefCode="+prefcode,
+      type: "GET",
+      headers: {
+          "X-API-KEY": "aopG1WDpGAaaMtOYfNQ3hiJbpbkWOgcBXVum3Etb"
+      },
+      async: "false",
+      dataType: "json",
+      success: function(data, dataType) {
+        for(var i=0;i<data['result'].length;i++){
+          cities.push(data['result'][i]['cityCode']);
+        }
+        for (key in cities) {
+          diff(cities[key],prefcode);
+        }
+      }
+    });
+}
+
+function diff(citycode,prefcode){
+    var in_pre_num = null;
+    var all_pre_num = null;
+    var in_city_num = null;
+    var kenngai = null;
+    var kennnai = null;
+    var result = null;
+    var cityname;
+    var prefname;
+    $.ajax({
+        url:"https://opendata.resas-portal.go.jp/api/v1/partner/docomo/destination?year=2016&month=01&periodOfDay=1&periodOfTime=14&gender=-&ageRange=-&prefCodeDestination="+prefcode+"&cityCodeDestination="+citycode+"&prefCodeResidence=-&cityCodeResidence=-",
+        type: "GET",
+        headers: {
+            "X-API-KEY": "aopG1WDpGAaaMtOYfNQ3hiJbpbkWOgcBXVum3Etb"
+        },
+        async: "false",
+        dataType: "json",
+        success: function(data, dataType) {
+          all_pre_num = data["result"]["total"];
+          //県外人数のチェック
+          for (var i=0;i<47;i++) {
+            if (data["result"]["prefs"][i]["prefCode"] == prefcode) {
+              prefname = data["result"]["prefs"][i]["prefName"];
+              for (var v=0;v<100;v++) {
+                if(data["result"]["prefs"][i]["cities"][v]["cityCode"] == citycode){
+                  in_city_num = data["result"]["prefs"][i]["cities"][v]['total'];
+                  cityname = data["result"]["prefs"][i]["cities"][v]["cityName"];
+                  break;
+                }else{
+                  continue;
+                }
+              }
+              in_pre_num = data["result"]["prefs"][i]["total"];
+              kenngai = all_pre_num - in_pre_num;
+              kennnai = all_pre_num - in_city_num - kenngai;
+              result = kennnai - kenngai;
+              var prefcity = prefname + cityname;
+              //console.log(citycode+","+prefname+cityname+",県外"+kenngai+",県内"+kennnai+",差分"+result);
+              var obj = {};
+              obj['name'] = prefcity;
+              obj['diff'] = result;
+              diff_secpop.push(obj);
+              break;
+            }else{
+              continue;
+            }
+          }
+        }
+    });
+}
+citycode_get(30);
+
+//ajaxが終わったらソート
+$(document).ajaxStop(function() {
+  var popular_spot = [];
+  var secret_spot = [];
+  diff_secpop.sort(function(a,b){
+    if(a.diff<b.diff) return -1;
+    if(a.diff > b.diff) return 1;
+    return 0;
+  });
+  for(var i=0;i<5;i++){
+    popular_spot.push(diff_secpop[i]['name']);
+  }
+  for(var i=diff_secpop.length-1;i>diff_secpop.length-6;i--){
+    secret_spot.push(diff_secpop[i]['name']);
+  }
+  var obj = {};
+  obj['secret'] = secret_spot;
+  obj['popular'] = popular_spot;
+  set_spot(obj);
+  console.log(obj);
+});
